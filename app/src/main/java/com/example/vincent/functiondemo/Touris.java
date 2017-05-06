b8782c84c7841e56fa57e2b84d1232042f972e69
Vincent Tai <rabrador2011@gmail.com>
2017-05-06 15:02:09 +0800

    loss Touris.java

---
+++ app/src/main/java/com/example/vincent/functiondemo/Touris.java
@@ -0,0 +1,50 @@
+package com.example.vincent.functiondemo;
+
+/**
+ * Created by Vincent on 2017/5/4.
+ */
+
+public class Touris {
+    String Data;
+    String Name;
+    String Title;
+    String Introduction;
+
+    public Touris(String name, String title, String introduction) {
+        Name = name;
+        Title = title;
+        Introduction = introduction;
+    }
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String data) {
+        Data = data;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getIntroduction() {
+        return Introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        Introduction = introduction;
+    }
+}
