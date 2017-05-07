d4de218d9d83603384132518166963597f4dc1f8
Vincent Tai <rabrador2011@gmail.com>
2017-05-07 11:53:00 +0800

    1. add four imagebutton 2. add ListActivity

---
+++ app/src/main/java/com/example/vincent/functiondemo/ListActivity.java
@@ -0,0 +1,83 @@
+package com.example.vincent.functiondemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+    ArrayList<Touris> dbTouris = new ArrayList<>();
+    private String data;
+    private ListView mList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        initView();
+
+        // Load Raw file and covert to String
+        covRawToString(loadRawFile());
+
+        // Parser json data
+        parserJson(data);
+
+        // insert to ListView
+        String[] namesArr = new String[dbTouris.size()];
+
+        for (int i = 0; i < dbTouris.size(); i++) {
+            namesArr[i] = dbTouris.get(i).getName();
+        }
+
+        mList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, namesArr));
+    }
+
+    public InputStream loadRawFile() {
+        return getResources().openRawResource(R.raw.data);
+    }
+
+    public String covRawToString(InputStream inputStream) {
+
+        try {
+            int size = inputStream.available();
+
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            data = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    public void parserJson(String sJson) {
+        try {
+            JSONObject obj = new JSONObject(sJson);
+            JSONArray array = obj.getJSONArray("data");
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject subObj = array.getJSONObject(i);
+
+                Touris touris = new Touris(subObj.getString("Name"), subObj.getString("Title"), subObj.getString("Introduction"));
+                dbTouris.add(touris);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initView() {
+        mList = (ListView) findViewById(R.id.mainList);
+    }
+}
--- app/src/main/java/com/example/vincent/functiondemo/MainActivity.java
+++ app/src/main/java/com/example/vincent/functiondemo/MainActivity.java
@@ -1,22 +1,17 @@
 package com.example.vincent.functiondemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Touris> dbTouris = new ArrayList<>();
-    private String data;
-    private ListView mList;
+
+    private ImageButton btnT1;
+    private ImageButton btnT2;
+    private ImageButton btnT3;
+    private ImageButton btnT4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,61 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
 
-        // Load Raw file and covert to String
-        covRawToString(loadRawFile());
-
-        // Parser json data
-        parserJson(data);
-
-        // insert to ListView
-        String[] namesArr = new String[dbTouris.size()];
-
-        for (int i = 0; i < dbTouris.size(); i++) {
-            namesArr[i] = dbTouris.get(i).getName();
-        }
-
-        mList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, namesArr));
-    }
-
-    public InputStream loadRawFile() {
-        return getResources().openRawResource(R.raw.data);
-    }
-
-    public String covRawToString(InputStream inputStream) {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-        try {
-            int size = inputStream.available();
-
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            data = new String(buffer, "UTF-8");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return data;
-    }
-
-    public void parserJson(String sJson) {
-        try {
-            JSONObject obj = new JSONObject(sJson);
-            JSONArray array = obj.getJSONArray("data");
-
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject subObj = array.getJSONObject(i);
-
-                Touris touris = new Touris(subObj.getString("Name"), subObj.getString("Title"), subObj.getString("Introduction"));
-                dbTouris.add(touris);
+        /* List View */
+        btnT2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intList = new Intent();
+                intList.setClass(MainActivity.this, ListActivity.class);
+                startActivity(intList);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
+
+
+
     }
 
     private void initView() {
-        mList = (ListView) findViewById(R.id.mainList);
+        btnT1 = (ImageButton) findViewById(R.id.imgBtnT1);
+        btnT2 = (ImageButton) findViewById(R.id.imgBtnT2);
+        btnT3 = (ImageButton) findViewById(R.id.imgBtnT3);
+        btnT4 = (ImageButton) findViewById(R.id.imgBtnT4);
     }
 }
