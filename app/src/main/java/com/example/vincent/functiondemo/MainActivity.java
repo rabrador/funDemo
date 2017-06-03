b51dfc53762e72e1c61fcd7751717aa0e1167c3c
Vincent Tai <rabrador2011@gmail.com>
2017-06-03 15:50:21 +0800

    connect to EDU for count

--- app/src/main/java/com/example/vincent/functiondemo/MainActivity.java
+++ app/src/main/java/com/example/vincent/functiondemo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.vincent.functiondemo;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+
+        /* Connect to EDU */
+        new connectEDU().execute("http://140.115.197.16/?school=nptu&app=pingtungtravel&year=106");
 
         /* Navigation */
         btnT1.setOnClickListener(new View.OnClickListener() {
@@ -56,5 +64,37 @@ public class MainActivity extends AppCompatActivity {
         btnT2 = (ImageButton) findViewById(R.id.imgBtnT2);
         btnT3 = (ImageButton) findViewById(R.id.imgBtnT3);
         btnT4 = (ImageButton) findViewById(R.id.imgBtnT4);
+    }
+
+    private class connectEDU extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d("TAG", "The response is: " + response);
+
+            } catch (Exception e) {
+                //ERROR
+                //Toast.makeText(MainActivity.this, "Connect ERROR", Toast.LENGTH_LONG).show();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_LONG).show();
+            super.onPostExecute(s);
+        }
+        //String url = "http://140.115.197.16/?school=nptu&app=pingtungtravel&year=106";
+
     }
 }
